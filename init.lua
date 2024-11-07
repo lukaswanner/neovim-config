ddd8830cc4a79851509b9ee1d7cef84c876bdc3f
Lukas Wanner <lukas_wanner@outlook.de>
2024-11-07 12:46:55 +0100

    added lsp lines

--- init.lua
+++ init.lua
@@ -7,19 +7,18 @@ vim.g.mapleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    import = "custom/plugins",
-    change_detection = { notify = false }
+	import = "custom/plugins",
+	change_detection = { notify = false },
 })
-
