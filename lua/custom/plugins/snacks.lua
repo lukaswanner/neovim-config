a29650a7dc1d4b2a9be69914c8ad4ebadc22e056
Lukas Wanner <lwanner@north.io>
2024-12-13 10:50:26 +0000

    added terminal from snacks

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -9,6 +9,7 @@ return {
 			notifier = { enabled = true },
 			quickfile = { enabled = true },
 			words = { enabled = true, notify_jump = true },
+			terminal = { enabled = true },
 		}
 		require("snacks").setup(opts)
 
@@ -18,6 +19,11 @@ return {
 
 		vim.keymap.set("n", "<leader>n", function()
 			require("snacks").words.jump(1, true)
+		end)
+
+		vim.keymap.set("n", "<leader>to", function()
+			-- if i ever need a specific command i can post this here instead of nil, example would be ls
+			require("snacks").terminal.toggle(nil, { win = { position = "float" } })
 		end)
 	end,
 }
