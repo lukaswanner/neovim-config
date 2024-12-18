2fb52b895faa820df8df2ae24c8886178d1125c6
Lukas Wanner <lukas_wanner@outlook.de>
2024-12-18 16:22:54 +0100

    added leetcode plugin

---
+++ lua/custom/plugins/leetcode.lua
@@ -0,0 +1,14 @@
+return {
+	"kawre/leetcode.nvim",
+	build = ":TSUpdate html", -- if you have `nvim-treesitter` installed
+	dependencies = {
+		"nvim-telescope/telescope.nvim",
+		"nvim-treesitter/nvim-treesitter",
+		-- "ibhagwan/fzf-lua",
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim",
+	},
+	opts = {
+		-- configuration goes here
+	},
+}
