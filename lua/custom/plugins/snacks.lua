f832f790bf7d93ccecfa5be3ecc8929ebf11f72c
Lukas Wanner <lukas_wanner@outlook.de>
2024-12-19 08:40:02 +0100

    remove terminal

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -8,13 +8,7 @@ return {
 			indent = { enabled = true, animate = { enabled = false } },
 			notifier = { enabled = true },
 			quickfile = { enabled = true },
-			terminal = { enabled = true },
 		}
 		require("snacks").setup(opts)
-
-		vim.keymap.set("n", "<leader>to", function()
-			-- if i ever need a specific command i can post this here instead of nil, example would be ls
-			require("snacks").terminal.toggle(nil, { win = { position = "float" } })
-		end)
 	end,
 }
