419c49aa5e2ca95f106dd4ace9a6b7065d55ea29
Lukas Wanner <lukas_wanner@outlook.de>
2025-01-31 10:17:03 +0100

    fixed keybindings for completions

--- lua/custom/completion.lua
+++ lua/custom/completion.lua
@@ -16,9 +16,9 @@ cmp.setup({
 	-- No, but seriously. Please read `:help ins-completion`, it is really good!
 	mapping = cmp.mapping.preset.insert({
 		-- Select the [n]ext item
-		["<C-n>"] = cmp.mapping.select_next_item(),
+		["<C-j>"] = cmp.mapping.select_next_item(),
 		-- Select the [p]revious item
-		["<C-p>"] = cmp.mapping.select_prev_item(),
+		["<C-k>"] = cmp.mapping.select_prev_item(),
 
 		-- Scroll the documentation window [b]ack / [f]orward
 		["<C-b>"] = cmp.mapping.scroll_docs(-4),
