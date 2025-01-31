bd0feac942f8c22b8c140d011fad391c3e2a805d
Lukas Wanner <lukas_wanner@outlook.de>
2025-01-31 10:06:43 +0100

    updated completion

--- lua/custom/completion.lua
+++ lua/custom/completion.lua
@@ -1,47 +1,70 @@
-vim.opt.completeopt = { "menu", "menuone", "noselect" }
-vim.opt.shortmess:append("c")
-
-local lspkind = require("lspkind")
-lspkind.init({})
-
 local cmp = require("cmp")
+local luasnip = require("luasnip")
+luasnip.config.setup({})
 
 cmp.setup({
-	sources = {
-		{ name = "nvim_lsp" },
-		{ name = "path" },
-		{ name = "buffer" },
-	},
-	mapping = {
-		["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-		["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-		["<C-y>"] = cmp.mapping.confirm({ select = true }),
-		["<enter>"] = cmp.mapping.confirm({ select = true }),
-		["<C-Space>"] = cmp.mapping.complete(),
-	},
-
-	-- Enable luasnip to handle snippet expansion for nvim-cmp
 	snippet = {
 		expand = function(args)
-			require("luasnip").lsp_expand(args.body)
+			luasnip.lsp_expand(args.body)
 		end,
 	},
+	completion = { completeopt = "menu,menuone,noinsert" },
+
+	-- For an understanding of why these mappings were
+	-- chosen, you will need to read `:help ins-completion`
+	--
+	-- No, but seriously. Please read `:help ins-completion`, it is really good!
+	mapping = cmp.mapping.preset.insert({
+		-- Select the [n]ext item
+		["<C-n>"] = cmp.mapping.select_next_item(),
+		-- Select the [p]revious item
+		["<C-p>"] = cmp.mapping.select_prev_item(),
+
+		-- Scroll the documentation window [b]ack / [f]orward
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+
+		-- Accept ([y]es) the completion.
+		--  This will auto-import if your LSP supports it.
+		--  This will expand snippets if the LSP sent a snippet.
+		["<C-y>"] = cmp.mapping.confirm({ select = true }),
+
+		-- If you prefer more traditional completion keymaps,
+		-- you can uncomment the following lines
+		--['<CR>'] = cmp.mapping.confirm { select = true },
+		--['<Tab>'] = cmp.mapping.select_next_item(),
+		--['<S-Tab>'] = cmp.mapping.select_prev_item(),
+
+		-- Manually trigger a completion from nvim-cmp.
+		--  Generally you don't need this, because nvim-cmp will display
+		--  completions whenever it has completion options available.
+		["<C-Space>"] = cmp.mapping.complete({}),
+
+		-- Think of <c-l> as moving to the right of your snippet expansion.
+		--  So if you have a snippet that's like:
+		--  function $name($args)
+		--    $body
+		--  end
+		--
+		-- <c-l> will move you to the right of each of the expansion locations.
+		-- <c-h> is similar, except moving you backwards.
+		["<C-l>"] = cmp.mapping(function()
+			if luasnip.expand_or_locally_jumpable() then
+				luasnip.expand_or_jump()
+			end
+		end, { "i", "s" }),
+		["<C-h>"] = cmp.mapping(function()
+			if luasnip.locally_jumpable(-1) then
+				luasnip.jump(-1)
+			end
+		end, { "i", "s" }),
+
+		-- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
+		--    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+	}),
+	sources = {
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
+		{ name = "path" },
+	},
 })
-
-local ls = require("luasnip")
-ls.config.set_config({
-	history = false,
-	updateevents = "TextChanged,TextChangedI",
-})
-
-vim.keymap.set({ "i", "s" }, "<c-k>", function()
-	if ls.expand_or_jumpable() then
-		ls.expand_or_jump()
-	end
-end, { silent = true })
-
-vim.keymap.set({ "i", "s" }, "<c-j>", function()
-	if ls.jumpable(-1) then
-		ls.jump(-1)
-	end
-end, { silent = true })
--- lua/custom/plugins/completion.lua
+++ lua/custom/plugins/completion.lua
@@ -1,18 +1,32 @@
 return {
-  {
-    "hrsh7th/nvim-cmp",
-    lazy = false,
-    priority = 100,
-    dependencies = {
-      "onsails/lspkind.nvim",
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-buffer",
-      { "L3MON4D3/LuaSnip", build = "make install_jsregexp" },
-      "saadparwaiz1/cmp_luasnip",
-    },
-    config = function()
-      require "custom.completion"
-    end,
-  },
+	{
+		"hrsh7th/nvim-cmp",
+		lazy = false,
+		priority = 100,
+		dependencies = {
+			{
+				"L3MON4D3/LuaSnip",
+				build = (function()
+					if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+						return
+					end
+					return "make install_jsregexp"
+				end)(),
+				dependencies = {
+					{
+						"rafamadriz/friendly-snippets",
+						config = function()
+							require("luasnip.loaders.from_vscode").lazy_load()
+						end,
+					},
+				},
+			},
+			"saadparwaiz1/cmp_luasnip",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-path",
+		},
+		config = function()
+			require("custom.completion")
+		end,
+	},
 }
